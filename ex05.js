7f9f0b562e89978107bd7b8c8314e2842c67337a
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-10-03 11:58:45 -0700

    Completed exercise 5

--- ex05.js
+++ ex05.js
@@ -2,8 +2,11 @@
 
 const _ = require('lodash');
 
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 5];
 
-const product = _.reduce(numbers)
+const product = _.reduce(numbers, (num, current) => {
+    multiplication = num * current
+    return multiplication
+}, 1)
 
 console.log(product); // Expected output: 24
